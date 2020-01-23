df28529eade6dc77d187f1c237cbaee2c00c2165
Paulo Sampaio <paulohenrique.sales47@gmail.com>
2020-01-22 21:47:43 -0300

    Section 12 - Utilizando AlertDialog

--- lib/Home.dart
+++ lib/Home.dart
@@ -18,6 +18,37 @@ class _HomeState extends State<Home> {
     }
   }
 
+  void _exibirDialog(BuildContext context, String titulo, String conteudo){
+    showDialog(
+      context: context,
+      builder: (context){
+        return AlertDialog(
+          title: Text(titulo,
+          style: TextStyle(
+            color: Colors.indigoAccent
+          ),),
+          content: Text(conteudo),
+          titlePadding: EdgeInsets.all(25),
+          backgroundColor: Colors.white,
+          actions: <Widget>[
+            RaisedButton(
+              onPressed: (){
+                Navigator.pop(context);
+              },
+              color: Colors.blue,
+              textColor: Colors.white,
+              child: Text("Sim",
+                style: TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 18
+              ),),
+
+            )
+          ],
+        );
+      });
+  }
+
   @override
   Widget build(BuildContext context) {
 
@@ -33,10 +64,16 @@ class _HomeState extends State<Home> {
           itemCount: _itens.length,
           itemBuilder: (context, indice){
             Map<String, dynamic> item = _itens[indice];
-
             return ListTile(
               title: Text(item["titulo"]),
               subtitle: Text(item["descricao"]),
+              onTap: (){
+                _exibirDialog(
+                    context,
+                    item["titulo"],
+                    item["descricao"]
+                );
+              },
             );
           })
 
